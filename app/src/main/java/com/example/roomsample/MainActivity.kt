f0bbfd9d5fa52088bb540b57f0db7b6029784503
maotea <maotea@gmail.comuser.name>
2020-11-14 17:11:53 +0800

    构建好了RecyclerView

--- app/src/main/java/com/example/roomsample/MainActivity.kt
+++ app/src/main/java/com/example/roomsample/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.roomsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.roomsample.adapter.WordListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerview.apply {
+            adapter = WordListAdapter()
+            layoutManager = LinearLayoutManager(this.context)
+        }
     }
 }
